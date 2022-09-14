8a7882b3149007f97c5a3484f152cb582f5fc21f
UpbeatOctopus <mason.thomas.kevin@gmail.com>
2022-09-14 19:01:48 -0400

    looks like mostly formating?

--- math_flash.py
+++ math_flash.py
@@ -1,14 +1,16 @@
 import numpy as np
-import pandas as pd 
-import calplot 
+import pandas as pd
+import calplot
 import matplotlib.pyplot as plt
 import pickle
 import os.path
 from os import path
 from random import randrange, choice
+
 # from tabulate import tabulate
-from prettytable import PrettyTable 
+from prettytable import PrettyTable
 from argparse import ArgumentParser
+
 ## https://stackoverflow.com/questions/3756278/timing-recording-input-in-python-3-1
 import time
 import datetime
@@ -17,8 +19,7 @@ import datetime
 from tinydb import Query, TinyDB, where
 from tinydb.operations import delete
 from pathlib import Path
-import pandas as pd 
-
+import pandas as pd
 
 
 ## TODO: Fix up database sytem move from pickle to tinydb
@@ -79,7 +80,7 @@ parser.add_argument(
     "-pd",
     "--dates_average",
     nargs=2,
-    metavar=('start_date', 'end_date'),
+    metavar=("start_date", "end_date"),
     dest="dates_average",
     action="store",
     help="Print the average times between two dates",
@@ -88,13 +89,12 @@ parser.add_argument(
 parser.add_argument(
     "-pr",
     "--report_range",
-    nargs='?',
-    const='10',
+    nargs="?",
+    const="10",
     dest="averages_days",
     action="store",
     help="Print the average time for a x days",
 )
-
 
 
 def open_db():
@@ -108,14 +108,15 @@ def open_db():
         # print("Directory ", local_storage, " already exists")
         pass
 
-    db = TinyDB(local_storage + '/db.json')
-    db.DEFAULT_TABLE = 'math_flash_table'
+    db = TinyDB(local_storage + "/db.json")
+    db.DEFAULT_TABLE = "math_flash_table"
     d_Query = Query()
     data = db.all()
     return db, d_Query
 
+
 def timed_input(prompt, start=0):
-    if (start == 0):
+    if start == 0:
         start = time.time()
     s = input(prompt)
     try:
@@ -123,148 +124,213 @@ def timed_input(prompt, start=0):
         return s, time.time() - start
     except ValueError:
         print("Please input int's only \n")
-        timed_input(prompt, start)
+        s = ""
+        ## .... recursive returns you say?
+        return timed_input(prompt, start)
+
 
 def gen_date_str():
     ## When does your day end?
-    dayend_offset_hours = '3'
+    dayend_offset_hours = "3"
     from datetime import datetime, timedelta
+
     datestr = datetime.now()
-    datestr = datestr - timedelta(hours = int(dayend_offset_hours)) 
+    datestr = datestr - timedelta(hours=int(dayend_offset_hours))
     datestr = str(datestr)
     timestr = datestr[11:-7]
     datestr = datestr[0:-16]
     return datestr, timestr
 
-def rand_num_by_len(length=2):
-    top = 10**length
-    return randrange(1,top)
 
-def print_day_average(db, d_Query, datestr): 
+def rand_num_by_len(length=2):
+    top = 10 ** length
+    return randrange(1, top)
+
+
+def print_day_average(db, d_Query, datestr):
 
     try:
-        days_results = db.get(d_Query.date  == datestr)
+        days_results = db.get(d_Query.date == datestr)
     except:
-        print(f'No data for {datestr}')
+        print(f"No data for {datestr}")
         return None
 
-
     if days_results is not None:
-        print('-------------')
+        print("-------------")
         print(datestr)
-        times = { "1" : [], "2" : [], "3": [], "4" : []}
-        for a in range(0,len(days_results['iterations'])):
-            for b in range(0,len(days_results['iterations'][a]['results'])):
-                problem_type = int(days_results['iterations'][a]['results'][f"{b}"][1])
-                time_taken = float(days_results['iterations'][a]['results'][f"{b}"][6])
+        times = {"1": [], "2": [], "3": [], "4": []}
+        for a in range(0, len(days_results["iterations"])):
+            for b in range(0, len(days_results["iterations"][a]["results"])):
+                problem_type = int(days_results["iterations"][a]["results"][f"{b}"][1])
+                time_taken = float(days_results["iterations"][a]["results"][f"{b}"][6])
                 times[f"{problem_type}"].append(time_taken)
 
-        for a in range(1,5):
+        for a in range(1, 5):
             if len(times[f"{a}"]) != 0:
-                avg_time = sum(times[f"{a}"])/len(times[f"{a}"])
+                avg_time = sum(times[f"{a}"]) / len(times[f"{a}"])
                 print(f" Problem type: {a} Average Time: {avg_time}")
     else:
         print(f"{datestr} has no data")
 
+
 def print_range(db, d_Query, start_date, end_date):
-    date_range = pd.date_range(start_date,end_date,freq='d')
+    date_range = pd.date_range(start_date, end_date, freq="d")
     for date in date_range:
-            #Format each date
-            date = str(date)
-            date = date[0:10]
-            # date = date.replace('-','')
-            print_day_average(db, d_Query, date)
+        # Format each date
+        date = str(date)
+        date = date[0:10]
+        # date = date.replace('-','')
+        print_day_average(db, d_Query, date)
+
 
 def save_results(db, d_Query, problem_index, datestr, timestr):
-    if db.get(d_Query.date  == datestr):
-        temp_storage = db.get(d_Query.date  == datestr)
-        temp_storage['iterations'].append({
-            'timestamp' : timestr  , 
-            'results': problem_index 
-            })
-        db.update({
-            'date': datestr,  
-            'iterations' :  temp_storage['iterations']
-            }, d_Query.date  == datestr)
+    if db.get(d_Query.date == datestr):
+        temp_storage = db.get(d_Query.date == datestr)
+        temp_storage["iterations"].append(
+            {"timestamp": timestr, "results": problem_index}
+        )
+        db.update(
+            {"date": datestr, "iterations": temp_storage["iterations"]},
+            d_Query.date == datestr,
+        )
     else:
-        db.insert({
-            'date': datestr,  
-            'iterations' : [{
-                'timestamp' : timestr  , 
-                'results': problem_index 
-                }]
-        })
-    
-def correct_answer(typ,num1,num2):
+        db.insert(
+            {
+                "date": datestr,
+                "iterations": [{"timestamp": timestr, "results": problem_index}],
+            }
+        )
+
+
+## Correct_answer shouldnt be returning the problem type or the numbers?
+def correct_answer(typ, num1, num2):
     if typ == 3:
         print("Problem type: Multiplication")
-        answer = num1*num2
+        answer = num1 * num2
     elif typ == 4:
         print("Problem type: Division")
         print("No decimal places at the moment, will add later.")
-        answer = int(num1/num2)
+        answer = int(num1 / num2)
     elif typ == 1:
         print("Problem type: Addition")
         ## Maybe do a reordering such that no negative answers
-        answer =  num1+num2
+        answer = num1 + num2
     elif typ == 2:
         print("Problem type: Subtraction")
-        answer =  num1-num2
+        answer = num1 - num2
     elif typ == 5:
-        # print("Problem type: Mixed")
+        print("Problem type: Mixed")
         ## yay recursion
-        typ,num1,num2,answer = correct_answer(choice((1,2)),num1,num2)
-    return typ,num1,num2,answer
+        typ, num1, num2, answer = correct_answer(choice((1, 2)), num1, num2)
+    elif typ == 6:
+        print("Problem type: base10 to binary")
+        answer = format(num1, "b")
+    elif typ == 7:
+        print("Problem type: binary to base10")
+        answer = num1
+    return typ, num1, num2, answer
+
 
 def show_problems(db, d_Query):
     total_time = 0
     correct = 0
-    for problem_number in range(0,args.number_of_problems):
-        numbers = [rand_num_by_len(args.number_of_digits),rand_num_by_len(args.number_of_digits)]
-        ## This is ugly but it allows for us to get zero padded digits.
-        print(f'{numbers[0]}'.zfill(args.number_of_digits) + '\n' + f'{numbers[1]}'.zfill(args.number_of_digits) )
+    for problem_number in range(0, args.number_of_problems):
+        numbers = [
+            rand_num_by_len(args.number_of_digits),
+            rand_num_by_len(args.number_of_digits),
+        ]
+        if args.type_of_problem == 6:
+            ## This is ugly but it allows for us to get zero padded digits.
+            print(f"{numbers[0]}".zfill(args.number_of_digits))
 
-        problem_type,num1,num2,answer = correct_answer(args.type_of_problem,numbers[0],numbers[1])
+            problem_type, num1, num2, answer = correct_answer(
+                args.type_of_problem, numbers[0], 0
+            )
+        elif args.type_of_problem == 7:
+            print(format(numbers[0], "b"))
 
-        s, thetime = timed_input('Answer?: ')
+        else:
+            print(
+                f"{numbers[0]}".zfill(args.number_of_digits)
+                + "\n"
+                + f"{numbers[1]}".zfill(args.number_of_digits)
+            )
 
-        if s == answer:
-            print('Correct!')
+        problem_type, num1, num2, answer = correct_answer(
+            args.type_of_problem, numbers[0], numbers[1]
+        )
+
+        s, thetime = timed_input("Answer?: ")
+        print(s)
+        print(answer)
+        if int(s) == int(answer):
+            print("Correct!")
             mark = "Correct"
             correct += 1
         else:
-            print('Incorrect! Answer was: {}'.format(answer))
+            print("Incorrect! Answer was: {}".format(answer))
             mark = "Incorrect"
-        
-        problem_index[problem_number] = [problem_number,problem_type,numbers[0],numbers[1],answer,s,str(thetime)[0:5],mark ]
 
-        x.add_row([problem_number,problem_type,numbers[0],numbers[1],answer,s,str(thetime)[0:5],mark ])
+        problem_index[problem_number] = [
+            problem_number,
+            problem_type,
+            numbers[0],
+            numbers[1],
+            answer,
+            s,
+            str(thetime)[0:5],
+            mark,
+        ]
+
+        x.add_row(
+            [
+                problem_number,
+                problem_type,
+                num1,
+                num2,
+                answer,
+                s,
+                str(thetime)[0:5],
+                mark,
+            ]
+        )
         total_time += thetime
-    
-    save_results(db, d_Query, problem_index, datestr,timestr)
-    ## Add summary AFTER putting into the database
-    problem_index['summary']= f'Got {correct} out of {args.number_of_problems}. in {total_time:02.2f} Seconds, which is {total_time/60:02.2f} minutes total and {(total_time)/args.number_of_problems:02.2f} seconds per problem  {(correct/args.number_of_problems)*100}%'
-    print(x)
-    print(problem_index['summary'])
 
+    save_results(db, d_Query, problem_index, datestr, timestr)
+    ## Add summary AFTER putting into the database
+    problem_index[
+        "summary"
+    ] = f"Got {correct} out of {args.number_of_problems}. in {total_time:02.2f} Seconds, which is {total_time/60:02.2f} minutes total and {(total_time)/args.number_of_problems:02.2f} seconds per problem  {(correct/args.number_of_problems)*100}%"
+    print(x)
+    print(problem_index["summary"])
 
 
 if __name__ == "__main__":
     datestr, timestr = gen_date_str()
     x = PrettyTable()
-    x.field_names =  [ "problem_number","problem_type","first","second","Answer","Input","Time Taken","Result" ]
+    x.field_names = [
+        "problem_number",
+        "problem_type",
+        "first",
+        "second",
+        "Answer",
+        "Input",
+        "Time Taken",
+        "Result",
+    ]
     problem_index = {}
     args = parser.parse_args()
     correct = 0
     total_time = 0
     db, d_Query = open_db()
 
-
     if args.print_averages:
         print_day_average(db, d_Query, args.print_averages)
     elif args.averages_days:
         ## pr
-        start_date = datetime.datetime.now() - datetime.timedelta(days=int(args.averages_days))
+        start_date = datetime.datetime.now() - datetime.timedelta(
+            days=int(args.averages_days)
+        )
         start_date = start_date.strftime("%Y-%m-%d")
         print_range(db, d_Query, start_date, datestr)
     elif args.dates_average:
@@ -272,4 +338,3 @@ if __name__ == "__main__":
         print_range(db, d_Query, args.print_averages[0], args.print_averages[1])
     else:
         show_problems(db, d_Query)
-
